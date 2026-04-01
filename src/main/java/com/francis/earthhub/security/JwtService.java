8721637dfce6273c17381ef47db5fb0476371f41
Francis <francisgacutno39@gmail.com>
2026-04-01 19:15:14 +0800

    feat: implement JWT-based authentication and authorization system
    - Added `JwtService` to handle JWT token generation, validation, and extraction.
    - Introduced `JwtAuthenticationFilter` to apply token-based authentication for incoming requests.
    - Updated `AppUser` to implement `UserDetails` for Spring Security integration.
    - Added `AuthController` with login endpoint to issue JWT tokens upon successful authentication.
    - Enhanced `SecurityConfig` to integrate `JwtAuthenticationFilter` and define public routes.
    - Created `AuthRequestDTO` and `AuthResponseDTO` to handle authentication requests and responses.
    - Updated EventService to fix organizer role validation logic.
    - Upgraded `io.jsonwebtoken` dependencies to version 0.12.5.

--- src/main/java/com/francis/earthhub/security/JwtService.java
+++ src/main/java/com/francis/earthhub/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
 
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
@@ -25,7 +27,7 @@ public class JwtService {
     }
 
     public SecretKey getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
